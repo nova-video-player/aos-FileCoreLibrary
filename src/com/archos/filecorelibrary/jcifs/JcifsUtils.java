bcd1f8e1d895839b10dd855dd238cd50ad1806ff
Courville Software <software@courville.org>
2020-12-11 07:34:05 +0100

    Revert "messing with jcifs.resolveOrder makes jcifs use wrong IP address"
    This reverts commit 429679eb8598fb39e9b8f3d82dd829cde25fedba.

--- src/com/archos/filecorelibrary/jcifs/JcifsUtils.java
+++ src/com/archos/filecorelibrary/jcifs/JcifsUtils.java
@@ -85,6 +85,10 @@ public class JcifsUtils {
         // must remain false to be able to talk to smbV1 only
         prop.put("jcifs.smb.client.useSMB2Negotiation", "false");
         prop.put("jcifs.smb.client.disableSMB1", "false");
+        // resolve in this order to avoid netbios name being also a foreign DNS entry resulting in bad resolution
+        // do not change resolveOrder for now
+        // TODO check if it has an impact: with jcifs-old, resolveOrder was not changed i.e. LMHOSTS,DNS,WINS,BCAST
+        prop.put("jcifs.resolveOrder", "BCAST,DNS");
         // get around https://github.com/AgNO3/jcifs-ng/issues/40 and this is required for guest login on win10 smb2
         prop.put("jcifs.smb.client.ipcSigningEnforced", "false");
         // allow plaintext password fallback
@@ -122,6 +126,12 @@ public class JcifsUtils {
 
         // get around https://github.com/AgNO3/jcifs-ng/issues/40 and this is required for guest login on win10 smb2
         prop.put("jcifs.smb.client.ipcSigningEnforced", "false");
+
+        // resolve in this order to avoid netbios name being also a foreign DNS entry resulting in bad resolution
+        // do not change resolveOrder for now
+        // TODO check if it has an impact: with jcifs-old, resolveOrder was not changed i.e. LMHOSTS,DNS,WINS,BCAST
+        prop.put("jcifs.resolveOrder", "BCAST,DNS");
+
         // allow plaintext password fallback
         prop.put("jcifs.smb.client.disablePlainTextPasswords", "false");
 
