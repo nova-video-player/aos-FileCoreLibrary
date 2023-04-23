7191a4b3ccedef115f17dac7c53f21099ccfc9ea
Courville Software <software@courville.org>
2023-04-23 12:14:37 +0200

    oops

--- src/com/archos/filecorelibrary/FileUtils.java
+++ src/com/archos/filecorelibrary/FileUtils.java
@@ -322,7 +322,7 @@ public class FileUtils {
         if (uri == null) return null;
         List<String> pathSegments = uri.getPathSegments();
         if (pathSegments != null && !pathSegments.isEmpty()) {
-            log.debug("getShareName: uri=" + uri + " -> " + Arrays.toString(pathSegments.toArray()) + " -> " +  pathSegments.get(0)));
+            log.debug("getShareName: uri=" + uri + " -> " + Arrays.toString(pathSegments.toArray()) + " -> " +  pathSegments.get(0));
             return pathSegments.get(0);
         }
         return null;
